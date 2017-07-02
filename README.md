851f197425fc63a155cda49217a3a6cf3d182ace
neonexxa <firdaus@cyberhax.club>
2017-07-02 12:47:28 +0800

    update ngram algorithm

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ngram-nexxa
-testing ngram python3
+# Ngram-nexxa
+Simple testing ngram python3
+
+## This package repositories has experimental data on ngram algorithm on text processing
+- [NLTK ngram library](https://raw.githubusercontent.com/neonexxa/ngram-nexxa/blob/master/nltksupport.py)
+- [Recursive ngram algorithm](https://raw.githubusercontent.com/neonexxa/ngram-nexxa/blob/master/recursivengram.py)
+
+## Illustration Demo
+![](https://thumbs.gfycat.com/MatureSolidIberianchiffchaff-size_restricted.gif)
+
+## Contributors
+Neonexxa
+
+## References
--- recursivengram.py
+++ recursivengram.py
@@ -1,17 +1,20 @@
-# array('output')
-def ngrams(input, n):
-    input = input.split(' ')
+def nlabel(x):
+    return {
+        1: 'Unigram',
+        2: 'Bigram',
+        3: 'Trigram',
+    }[x]
+
+# maininput denote the input text/sentence , while n denote the number of of Grams
+def ngrams(maininput, n):
+    input = maininput.split(' ')
     output = {}
     for i in range(len(input)-n+1):
         g = ' '.join(input[i:i+n])
         output.setdefault(g, 0)
         output[g] += 1
-    if n == 1 :
-        print (n)
-        print (output)
-    else:
-        print (n)
-        print (output)
-        return ngrams('abu like to read while he like to sleep', n-1)
+    print (nlabel(n) + str(output))
+    if n!=1 :
+        return ngrams(maininput, n-1)
     
 ngrams('abu like to read while he like to sleep', 3)
